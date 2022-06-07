28e7f1be9c6bbef578c4dc24ac29704cc0bf35c3
Vladimir Below <laitvladimir@yandex.ru>
2022-06-07 17:50:42 +0300

    Update: env

--- src/openDatabase.js
+++ src/openDatabase.js
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-// const supabaseUrl = 'https://ashwrraqigmhblzkrcos.supabase.co'
-// const service_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzaHdycmFxaWdtaGJsemtyY29zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1NDI0OTUwMCwiZXhwIjoxOTY5ODI1NTAwfQ.wEKnTzak9V2nYUk7cPjiEGr2up7AegKdDkSUG25PvVs'
-// export const supabase = createClient(supabaseUrl, service_key)
-export const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_KEY)
+console.log( process.env )
+
+const supabaseUrl = 'https://ashwrraqigmhblzkrcos.supabase.co'
+const service_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzaHdycmFxaWdtaGJsemtyY29zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1NDYwMTY2MiwiZXhwIjoxOTcwMTc3NjYyfQ.luS2XwXF2Qm-FfDlvafBTGYpB2LmX1V2Yszqg4Evjjw'
+export const supabase = createClient(supabaseUrl, service_key)
+// export const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_KEY)
 
