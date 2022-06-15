5b5109534cb1f2a440977872b0c353666e80c1e4
VIOOI <59562970+VIOOI@users.noreply.github.com>
2022-06-15 13:38:23 +0300

    Update openDatabase.js

--- src/openDatabase.js
+++ src/openDatabase.js
@@ -3,7 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 console.log( process.env )
 
 const supabaseUrl = 'https://ashwrraqigmhblzkrcos.supabase.co'
-const service_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzaHdycmFxaWdtaGJsemtyY29zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY1NTI4MjQ4NCwiZXhwIjoxOTcwODU4NDg0fQ.T4sTW5U071Co7NG0bfhu9Niexeoojk0N5xn-iIUdfP8'
+const service_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzaHdycmFxaWdtaGJsemtyY29zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTUyODg4NzUsImV4cCI6MTk3MDg2NDg3NX0.ba9UDmiW6M1Z_8Xu0wqEtNzzU2OC9rr4gWFM997uQY8'
 // export const supabase = createClient(supabaseUrl, service_key)
 export const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_KEY)
 
